7e37ca1a59a930a41b96f9552c172f820017760d
sandeepkumawat9829 <kumawatsandeep1st@gmail.com>
2024-11-01 21:14:13 +0530

    GFG POTD solution for November 01, 2024

---
+++ 2024/November/01-Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maxSum(Long[] arr) {
+        // code here
+        Arrays.sort(arr);
+        long sum = 0;
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
+            sum+= (Math.abs(arr[j]-arr[i++]) + Math.abs(arr[j--]-arr[i]));
+            
+        }
+        sum+= Math.abs(arr[i]-arr[0]);
+        return sum;
+    }
+}
