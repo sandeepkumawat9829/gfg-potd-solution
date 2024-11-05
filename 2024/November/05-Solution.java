79b0de600e14bce7edfd1fedce206898e4850a70
sandeepkumawat9829 <kumawatsandeep1st@gmail.com>
2024-11-05 14:10:08 +0530

    GFG POTD solution for November 05, 2024

---
+++ 2024/November/05-Solution.java
@@ -0,0 +1,19 @@
+class GFG {
+    static void rotate(int mat[][]) {
+        // Code Here
+        for(int i=0;i<mat.length;i++){
+            for(int j=i+1;j<mat.length;j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat.length/2;j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][mat.length-j-1];
+                mat[i][mat.length-j-1] = temp;
+            }
+        }
+    }
+}
