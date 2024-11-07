e6ad3a0837a099271732932275b7f236cc0f5570
sandeepkumawat9829 <kumawatsandeep1st@gmail.com>
2024-11-07 23:24:20 +0530

    GFG POTD solution for November 07, 2024

---
+++ 2024/November/07-Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public List<Integer> findSplit(int[] arr) {
+        // Return an array of possible answer, driver code will judge and return true or
+        // false based on
+        List<Integer> list=new ArrayList<>();
+        int sum=0;
+        for(int i:arr){
+            sum+=i;
+        }
+        if(sum%3!=0){
+            return Arrays.asList(-1,-1);
+        }
+        int res=sum/3;
+        int count=0;
+        sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum==res){
+                list.add(i);
+                sum=0;
+                if(list.size()==2) return list;
+            }
+        }
+        return Arrays.asList(-1,-1);
+    }
+}
