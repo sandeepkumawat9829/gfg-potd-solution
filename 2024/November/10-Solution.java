7c71f73fab584347fc48d3e9f982ec9e67b2b792
sandeepkumawat9829 <kumawatsandeep1st@gmail.com>
2024-11-10 22:18:15 +0530

    GFG POTD solution for November 10, 2024

---
+++ 2024/November/10-Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    // Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // add your code here
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : a) {
+            set.add(num);
+        }
+        for (int num : b) {
+            set.add(num);
+        }
+        return new ArrayList<>(set);
+    }
+}
