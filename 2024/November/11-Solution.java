fccde1ddd69e0938fd77f3383dd854a0f3d1ce86
sandeepkumawat9829 <kumawatsandeep1st@gmail.com>
2024-11-11 19:25:17 +0530

    GFG POTD solution for November 11, 2024

---
+++ 2024/November/11-Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minIncrements(int[] arr) {
+        // Code here
+        Arrays.sort(arr);
+        int ans=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<=arr[i-1]){
+                ans+=arr[i-1]-arr[i]+1;
+                arr[i]=arr[i-1] + 1;
+            }
+        }
+        return ans;
+    }
+}
